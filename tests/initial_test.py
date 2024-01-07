bd2f96c5d55dd013134878064e036a08e89ad442
bunjevac98 <basic.aco@hotmail.com>
2024-01-08 00:50:45 +0100

    First initial test

--- tests/initial_test.py
+++ tests/initial_test.py
@@ -1,11 +1,12 @@
 import httpx
 import pytest
-import asyncio
+
 from app.main import app
+
 
 @pytest.mark.asyncio
 async def test_root():
     async with httpx.AsyncClient(app=app, base_url="http://test") as client:
-        response= await client.get("/")
-    assert response.status_code ==200
+        response = await client.get("/")
+    assert response.status_code == 200
     assert response.json() == {"message": "Welcome to API"}
