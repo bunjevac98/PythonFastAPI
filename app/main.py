ac767b6e860e298c430f792471907fc470050578
bunjevac98 <basic.aco@hotmail.com>
2024-01-08 15:51:44 +0100

    Get/project implemented fith built-in data types

--- app/main.py
+++ app/main.py
@@ -1,10 +1,56 @@
+from typing import Optional
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
-# decorater method path
+# If we want to save pdf file it is bytes type in python
+
+
+class Project(BaseModel):
+    id: int
+    name: str
+    description: str
+    owner: int
+    team_members: Optional[list]
+    attachements: str
+
+
+my_projects = [
+    {
+        "id": "1",
+        "name": "Project for juniors",
+        "description": "This will be nice start for junior",
+        "owner": "1",
+        "team_members": ["1", "2", "3"],
+        "attachements": "some file",
+    },
+    {
+        "id": "2",
+        "name": "Project for mediors",
+        "description": "This will be nice start for mediors",
+        "owner": "1",
+        "team_members": ["2", "4", "5"],
+        "attachements": "Drag some file",
+    },
+]
 
 
 @app.get("/")
 async def root():
     return {"message": "Welcome to API"}
+
+
+@app.get("/projects")
+def get_projects():
+    return {"data": my_projects}
+
+
+@app.post("/projects")
+def create_project(project: Project):
+    print(project.name)
+    return {"data": "new project"}
+
+
+# title string, content string, category, bool
+# we can store whatever we want in that schema
